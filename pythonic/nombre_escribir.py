807aeb85581754a285c1d8713f445fb517df6442
Clara Jenhee <clara20franco@gmail.com>
2025-06-30 18:15:53 -0600

    Update and rename nombre.py to nombre_escribir.py

--- pythonic/nombre_escribir.py
+++ pythonic/nombre_escribir.py
@@ -2,9 +2,9 @@ nombre = input("¿Cuál es tu nombre? ")
 
 #en nombres.txt se van a escribir los nombres que ingresemos desde consola "a" es de append 
 # que se usa para añadir porque si hacemos un "w" de write escribir reescribe todo
-file = open("nombres.txt", "a")
-file.write(f"{nombre}\n") #escribir y tener un salto de linea entre lo escrito
-file.close() #cerrar el archivo
+with open("nombres.txt", "a") as file:
+    file.write(f"{nombre}\n") #escribir y tener un salto de linea entre lo escrito
+    file.close() #cerrar el archivo para que no se corrompa
 
 # Para ver el archivo en la terminal seria 
 # python nombre.py -correr el archivo-
