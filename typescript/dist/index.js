64703f910d89805a1646c04587d919ac24a644df
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-09-15 08:57:36 -0600

    delete dir typescript/

--- typescript/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let letras = "HOLA MUNDO";
-console.log(letras);
-//# sourceMappingURL=index.js.map
--- typescript/index.ts
+++
@@ -1,3 +0,0 @@
-let letras: string = "HOLA MUNDO";
-
-console.log(letras);
